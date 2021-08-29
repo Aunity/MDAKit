a743bad3ca5b08a62ea494ff26540bfb7ea7e3be
ymh@g01 <maohuay@hotmail.com>
2021-08-29 15:17:59 +0800

    add traj-align and pentapeptide example case

---
+++ example/pentapeptide/fetch_data.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+'''
+@Author ymh
+@Email  maohuay@hotmail.com
+@Date   2021-08-29 15:03:30
+@Web    https://github.com/Aunity
+'''
+
+import os,sys
+from mdshare import fetch
+fetch('pentapeptide-impl-solv.pdb')
+fetch('pentapeptide-*-500ns-impl-solv.xtc')
